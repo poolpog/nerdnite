513efbbacb80c63831877dafb0ef05823a7d1c05
Jeff Silverman <jeffrey.d.silverman@gmail.com>
2015-02-24 10:53:55 -0500

    initial nerdnite helper

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nerdnite
 Nerdnite helper libraries
+
+*Requirements*
+* Python
+
+1. Clone this repo
+2. Run ./nerdnite-helper.py
+3. Copy and paste generated URL into your browser
---
+++ nerdnite-helper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import re
+import base64
+
+uuu="aHR0c;ovL2dvGy5nGC9mG3Jtcy95c;F;GEw4Tkx0Cg@@"
+
+def nerdnite_helper_1(uuu):
+    www=re.sub(r'@',r'=',re.sub(r';',r'D',re.sub(r'G',r'b',uuu)))
+    return www
+
+def nerdnite_helper_2(uuu):
+    return base64.b64decode(uuu)
+
+########
+# MAIN #
+########
+def main():
+    print nerdnite_helper_2(nerdnite_helper_1(uuu))
+    exit( 0 )
+
+# Execute
+if __name__ == "__main__":
+    main()
